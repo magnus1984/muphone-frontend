e7df33d4cf833f5aa3bc6fa0d3d024216d6d0859
Jonathan Pelletier <jopela@hedgenet.ca>
2018-07-15 11:58:05 -0400

    adding pages and react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Heading from './components/heading'
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Heading> &mu;phone verification service; a prototype by <a style={{color:'white'}} href="https://hedgenet.info"> hedgenet.info </a>  </Heading>
+        <Heading> &mu;phone verification service; a prototype from the creator of <a style={{color:'white'}} href="https://hedgenet.info"> hedgenet.info </a>  </Heading>
         <h1> Hello world from application ! </h1>
       </div>
     )

3cbdbda473ffd25fd2e6d4f56c6e56c1fc85b733
Jonathan Pelletier <jopela@hedgenet.ca>
2018-07-15 20:32:21 -0400

    Adding full validation workflow

--- src/api/verification.js
+++ src/api/verification.js
@@ -2,20 +2,19 @@ import axios from 'axios'
 
 export function submitPhone(number) {
 
-    const endpoint = process.env.REACT_APP_API_ENDPOINT_URL
+    const endpoint = process.env.REACT_APP_API_ENDPOINT_URL + '/phone'
 
     return axios.post(endpoint, {
         number:number
     })
-    .then( (result) => {
-        console.log(result)
-    })
-    .catch( (err) => {
-        console.log(err)
-    })
-
 }
 
-export function submitPhoneVerification(number, code) {
-    return null
+export function submitPhoneVerification(number, validationCode) {
+
+    const endpoint = process.env.REACT_APP_API_ENDPOINT_URL + '/phone/validation'
+
+    return axios.post(endpoint, {
+        number:number,
+        validation_code:validationCode
+    })
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Route } from 'react-router-dom'
+
 import { Container, Row, Col } from 'reactstrap'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
@@ -8,54 +10,139 @@ import { submitPhone, submitPhoneVerification }  from '../api/verification'
 class Home extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {verificationState: 'init', verificationCode:''}
 
 
         this.onPhoneNumberChange = this.onPhoneNumberChange.bind(this)
         this.onSubmitButtonClick = this.onSubmitButtonClick.bind(this)
+        this.onVerificationCodeChange = this.onVerificationCodeChange.bind(this)
+        this.onVerificationButtonClick = this.onVerificationButtonClick.bind(this)
     }
 
     onPhoneNumberChange(number) {
         this.setState({'number':number})
     }
 
+    onVerificationCodeChange(verificationCode) {
+        this.setState({'verificationCode':verificationCode})
+    }
+
+    onVerificationButtonClick() {
+
+        submitPhoneVerification(this.state.number, this.state.verificationCode).then( (result) => {
+
+            if (result.data.validation_status == 'VALID') {
+
+                this.setState({'verificationState':'valid'})
+            } else {
+                this.setState({'verificationState':'invalid'})
+            }
+        })
+        .catch( () => { this.setState({'verificationState':'invalid'}) } )
+    }
+
     onSubmitButtonClick() {
 
         submitPhone(this.state.number).then( () => {
-            // navigate to other page and give the phone as parameter
-            console.log(`navigate to other page : ${this.state.number}`)
+                this.setState({verificationState:'new'})
             })
     }
 
     render() {
-        return (
+
+        if (this.state.verificationState === 'init') {
+
+            return (
+                    <Container>
+                        <Row>
+                            <Col>
+                                <div style={{textAlign:'center'}} >
+                                <h1> Phone Verification Form</h1>
+                                <p> please enter a valid phone number to verify </p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form>
+                                    <FormGroup>
+                                      <Label for="phoneNumber">Phone Number</Label>
+                                      <Input
+                                            onChange={(e) => this.onPhoneNumberChange(e.target.value)}
+                                            type="text"
+                                            name="phoneNumber"
+                                            id="phoneNumber"
+                                            placeholder="(e.g:+15147174993)" />
+                                    </FormGroup>
+                                    <Button color="success" onClick={() => this.onSubmitButtonClick() }>Submit</Button>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Container>
+                )
+        } else if (this.state.verificationState === 'new') {
+
+            return (
                 <Container>
                     <Row>
                         <Col>
-							<div style={{textAlign:'center'}} >
-                            <h1> Phone Verification Form</h1>
-                            <p> please enter a valid phone number to verify </p>
-							</div>
+                                <div style={{textAlign:'center'}} >
+                                <h1> Validation Code </h1>
+                                <p> Please Enter the validation code you received on your phone </p>
+                                </div>
                         </Col>
                     </Row>
-					<Row>
-						<Col>
-							<Form>
-                                <FormGroup>
-                                  <Label for="phoneNumber">Phone Number</Label>
-                                  <Input
-                                        onChange={(e) => this.onPhoneNumberChange(e.target.value)}
-                                        type="text"
-                                        name="phoneNumber"
-                                        id="phoneNumber"
-                                        placeholder="(e.g:+15147174993)" />
-                                </FormGroup>
-                                <Button onClick={() => this.onSubmitButtonClick() }>Submit</Button>
-                            </Form>
-						</Col>
-					</Row>
+                        <Row>
+                            <Col>
+                                <Form>
+                                    <FormGroup>
+                                      <Label for="validationCode">Validation Code</Label>
+                                      <Input
+                                            type="text"
+                                            name="validationCode"
+                                            id="validationCode"
+                                            value={this.state.verificationCode}
+                                            onChange={(e) => this.onVerificationCodeChange(e.target.value)}
+                                            placeholder="(e.g:123456)" />
+                                    </FormGroup>
+                                    <Button onClick={this.onVerificationButtonClick} color="success">Submit Verification</Button>
+                                </Form>
+                            </Col>
+                        </Row>
+
+                </Container>
+
+
+            )
+        } else if (this.state.verificationState === 'valid') {
+            return (
+
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1> Your phone number is now validated </h1>
+                        </Col>
+                    </Row>
                 </Container>
             )
+        }
+
+        else {
+
+            return (
+
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1> Invalid Code or non-existing phone number </h1>
+                        </Col>
+                    </Row>
+                </Container>
+            )
+
+
+        }
+
     }
 
 }
